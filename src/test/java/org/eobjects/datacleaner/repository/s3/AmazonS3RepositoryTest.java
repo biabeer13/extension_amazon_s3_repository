dc1241ed0d190daf1c10f2627fbb509f42bf2a46
kaspersorensen <i.am.kasper.sorensen@gmail.com>
2014-06-12 15:55:23 +0200

    Got a proper connection to localhost Riak CS.

--- src/test/java/org/eobjects/datacleaner/repository/s3/AmazonS3RepositoryTest.java
+++ src/test/java/org/eobjects/datacleaner/repository/s3/AmazonS3RepositoryTest.java
@@ -14,8 +14,6 @@ public class AmazonS3RepositoryTest extends TestCase {
 
     private static final String ACCESS_KEY = "NE7YIAXSWBO8UEINUSYF";
     private static final String SECRET_KEY = "PICc90ariL1I640_UcMu3kuIZkNs0zwlZxFjlA==";
-    private static final String PROXY_HOST = "192.168.0.141";
-    private static final int PROXY_PORT = 8080;
     
     private AmazonS3Repository _repo;
     
@@ -25,10 +23,10 @@ public class AmazonS3RepositoryTest extends TestCase {
         
         final AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
         final ClientConfiguration clientConfiguration = new ClientConfiguration();
-        clientConfiguration.setProxyHost(PROXY_HOST);
-        clientConfiguration.setProxyPort(PROXY_PORT);
+        clientConfiguration.setPreemptiveBasicProxyAuth(true);
         
         final AmazonS3Client client = new AmazonS3Client(credentials, clientConfiguration);
+        client.setEndpoint("http://localhost:8080");
         
         _repo = new AmazonS3Repository(client);
     }
@@ -36,7 +34,7 @@ public class AmazonS3RepositoryTest extends TestCase {
     public void testConnect() throws Exception {
         AmazonS3Client client =  _repo.getClient();
         List<Bucket> buckets = client.listBuckets();
-        assertEquals("", buckets.toString());
+        assertNotNull(buckets);
     }
     
 }
